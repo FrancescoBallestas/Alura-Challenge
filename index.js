6289edf244440fa26bbf13a6c342daafa43068cf
Francesco <franciballestas@gmail.com>
2023-01-24 13:59:10 -0500

    Modificaciones en JS

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 //Función para desaparecer texto antes de escribir
 function switchTxt() {
   var x = document.getElementById('switch-txt');
-  if (x.style.display === 'none') {
-    
-  } else {
+  if (x.style.display !== 'none') {
     x.style.display = 'none';
   }
+}
+
+var sar = document.getElementById('mostrar-mensaje');
+if (sar.style.display !== 'none') {
+  sar.style.display = "none";
 }
 
 //Función JavaScript para encriptar el texto
@@ -13,11 +16,7 @@ function encrypt() {
   let texto = document.getElementById("cuadro-texto").value;
   let encryptedString = texto.replace(/e/g, "enter").replace(/i/g, "imes").replace(/a/g, "ai").replace(/o/g, "ober").replace(/u/g, "ufat");
   document.getElementById("respuesta").innerHTML = encryptedString;
-  document.getElementById("cuadro-respuesta").style.display = "block";
-  var hiddenElements = document.getElementsByClassName("hidden");
-  for (var i = 0; i < hiddenElements.length; i++) {
-    hiddenElements[i].style.display = "none";
-  }
+  document.getElementById("mostrar-mensaje").style.display = "block";
   document.getElementById("copy").style.visibility = "visible";
 }
 
@@ -30,11 +29,7 @@ function desencrypt() {
   let texto = document.getElementById("cuadro-texto").value;
   let desencryptedString = texto.replace(/enter/g, "e").replace(/imes/g, "i").replace(/ai/g, "a").replace(/ober/g, "o").replace(/ufat/g, "u");
   document.getElementById("respuesta").innerHTML = desencryptedString;
-  document.getElementById("cuadro-respuesta").style.display = "block";
-  var hiddenElements = document.getElementsByClassName("hidden");
-  for (var i = 0; i < hiddenElements.length; i++) {
-    hiddenElements[i].style.display = "none";
-  }
+  document.getElementById("mostrar-mensaje").style.display = "block";
   document.getElementById("copy").style.visibility = "visible";
 }
 
@@ -42,7 +37,7 @@ function desencrypt() {
 var button = document.getElementById("btn-desencriptar");
 button.onclick = desencrypt;
 
-
+// Función para copiar el texto desencriptado
 function copiarTexto() {
   // Seleccionar el elemento de texto
   var texto = document.getElementById("respuesta").value;
@@ -50,18 +45,13 @@ function copiarTexto() {
   if(texto.trim() == ""){
     alert("No hay texto para copiar");
   } else {
-    navigator.clipboard.writeText(texto).then(function() {
-      alert("Texto copiado al portapapeles");
-    }, function(err) {
-      console.error('Error al copiar al portapapeles: ', err);
-    });
+    navigator.clipboard.writeText(texto);
   }
 }
 
-
+// Función para validar si hay texto por encriptar/desencriptar
 function validarCuadroTexto() {
-  let texto = document.getElementById("cuadro-texto").value;
-  
+  let texto = document.getElementById("cuadro-texto").value; 
   if(texto === ""){
     alert("No hay texto en el cuadro, por favor ingresa un texto");
     return false;
@@ -72,6 +62,7 @@ function validarCuadroTexto() {
     return true;
   }
 }
+
 //Asignar la función al botón
 var button = document.getElementById("btn-encriptar");
 button.onclick = function(){
@@ -87,12 +78,4 @@ button.onclick = function(){
         desencrypt();
     }
 };
-
-var mostrarBoton = true;
-var miBoton = document.getElementById("copy");
-if (mostrarBoton) {
-  miBoton.style.visibility = "visible";
-} else {
-  miBoton.style.visibility = "hidden";
-}
 
